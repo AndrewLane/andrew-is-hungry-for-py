e73642e60c467e8bffad72b979613a173b32dd1c
Andrew Lane <andrew.lane@hobsons.com>
2020-10-09 16:01:37 -0400

    Adding a dummy package implementation with some simple tests

---
+++ tests/test_eat.py
@@ -0,0 +1,11 @@
+import unittest
+import pytest
+
+from andrew_is_hungry_for_py.eat import feedme
+
+@pytest.mark.parametrize("input_date,expected", [
+    ("2019-10-31", "Birthday Cake"),
+    ("2019-10-01", "Cake"),
+])
+def test_feedme(input_date, expected):
+    assert feedme(input_date) == expected
