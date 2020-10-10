808c0b1d67c098a9679f23119ba8b8d4384c2f74
Andrew Lane <andrew.lane@hobsons.com>
2020-10-10 13:53:20 -0400

    Fix test

--- tests/test_eat.py
+++ tests/test_eat.py
@@ -7,7 +7,7 @@ from andrew_is_hungry_for_py.eat import feedme
 @pytest.mark.parametrize("input_date,expected", [
     ("2019-10-31", "Birthday Cake"),
     ("2019-10-01", "Cake"),
-    ("2019-11-01", "Leftover Birthday Cake__wrong_break_test"),
+    ("2019-11-01", "Leftover Birthday Cake"),
 ])
 def test_feedme(input_date, expected):
     assert feedme(input_date) == expected
