3e0e25b81451597c2b5bcc246e3f38cf6c61a22a
Andrew Lane <andrew.lane@hobsons.com>
2020-10-10 13:25:29 -0400

    Appease the linter

--- tests/test_eat.py
+++ tests/test_eat.py
@@ -3,6 +3,7 @@ import pytest
 
 from andrew_is_hungry_for_py.eat import feedme
 
+
 @pytest.mark.parametrize("input_date,expected", [
     ("2019-10-31", "Birthday Cake"),
     ("2019-10-01", "Cake"),
